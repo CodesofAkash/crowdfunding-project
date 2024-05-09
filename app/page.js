ddd70e2d5b8dfa9d60252bb1c6861150e7a5de74
Akash Sharma <akashcodesharma@gmail.com>
2024-05-09 15:14:50 +0530

    Update

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <div className="text-white p-4 flex flex-col gap-4 justify-center items-center h-[44vh]">
-        <div className="font-bold text-5xl flex justify-center items-center gap-10">Buy me a Chai. <span><Image className="w-20 h-25" src="/chai.gif"/></span></div>
+        <div className="font-bold text-5xl flex justify-center items-center gap-10">Buy me a Chai. <span><Image className="w-20 h-25" alt="error" src="/chai.gif"/></span></div>
         <p>A crowddfunding platform for creators. Get funded by your fans and followers.</p>
         <div>
           <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here</button>
