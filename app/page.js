2f61a67bcfdf32d7ed0ac7c4256f6e28ea79e990
Akash Sharma <akashcodesharma@gmail.com>
2024-05-09 14:13:21 +0530

    Update

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
         <div>
           <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here</button>
           <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read More</button>
-        </div>
+        </div> 
       </div>
 
       <div className="bg-white h-1 opacity-10"></div>
@@ -16,17 +16,17 @@ export default function Home() {
         <h1 className="text-3xl font-bold text-center mb-14">Your fans can buy you a Chai</h1>
         <div className="flex gap-5 justify-around">
           <div className="item space-y-3 flex flex-col justify-center items-center">
-            <img className="bg-slate-400 rounded-full p-2 text-black h-[88px] w-[88px]" src="/Get-me-a-Chai/public/man.gif" alt="" />
+            <img className="bg-slate-400 rounded-full p-2 text-black h-[88px] w-[88px]" src="/man.gif" alt="" />
             <p className="font-bold">Fans want to help</p>
             <p>Your fans are available for you to help you.</p>
           </div>
           <div className="item space-y-3 flex flex-col justify-center items-center">
-            <img className="bg-slate-400 rounded-full text-black h-[88px] w-[88px]" src="/Get-me-a-Chai/public/coin.gif" alt="" />
+            <img className="bg-slate-400 rounded-full text-black h-[88px] w-[88px]" src="/coin.gif" alt="" />
             <p className="font-bold">Fans want to help</p>
             <p>Your fans are available for you to help you.</p>
           </div>
           <div className="item space-y-3 flex flex-col justify-center items-center">
-            <img className="bg-slate-400 rounded-full p-2 text-black h-[88px] w-[88px]" src="/Get-me-a-Chai/public/group.gif" alt="" />
+            <img className="bg-slate-400 rounded-full p-2 text-black h-[88px] w-[88px]" src="/group.gif" alt="" />
             <p className="font-bold">Fans want to help</p>
             <p>Your fans are available for you to help you.</p>
           </div>
