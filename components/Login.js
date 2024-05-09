90be565b1cddd504db0ffeb567fc9dbb96dd9c58
Akash Sharma <akashcodesharma@gmail.com>
2024-05-09 10:27:54 +0530

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -32,7 +32,7 @@ const Login = () => {
                   </div>
                   <button type='submit' className="w-full text-white bg-primary-600 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-lg px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have an account yet? <Link href="/Get-me-a-Chai/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
+                      Don’t have an account yet? <Link href="/Get-me-a-Chai/app/signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
                   </p>
               </form>
           </div>
